ba651b6c808a2238a4be1b96939bd58b9d4b69a1
syys96 <3359652182@qq.com>
2022-01-30 19:57:24 +0800

    finish

--- botzone/merge.sh
+++ botzone/merge.sh
@@ -1,3 +1,11 @@
+cd ..
+git submodule update --init --recursive
+cd botzone
 python ../scripts/compile_proto.py ../libs/lczero-common/proto/net.proto \
         --proto_path=../libs/lczero-common --cpp_out=./
+/Users/syys/software/anaconda3/bin/python merge.py
+cd cmake-build-release
+cmake ..
+make
+
 
